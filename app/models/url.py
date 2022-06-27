f7723045e1369e410efef18d6f5a9026387f9ce2
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-06-26 20:09:54 -0500

    :bug: Random URL Key
    Create a funcion that call secrets function, this is because calling inside default value always give the same output

--- app/models/url.py
+++ app/models/url.py
@@ -1,16 +1,17 @@
 import secrets
+from typing import Callable
 
 from sqlalchemy import Boolean, Column, Integer, String
 
 from app.db.base_class import Base
 
+generate_url_key: Callable[[], str] = lambda: secrets.token_urlsafe(7)
+
 
 class URL(Base):
-    url: str = Column(
-        String(10), unique=True, index=True, default=secrets.token_urlsafe(7)
-    )
+    url: str = Column(String(10), unique=True, index=True, default=generate_url_key)
     admin_url: str = Column(
-        String(10), unique=True, index=True, default=secrets.token_urlsafe(7)
+        String(10), unique=True, index=True, default=generate_url_key
     )
     target_url: str = Column(String(255), unique=True, index=True)
     is_active: bool = Column(Boolean, default=True)
