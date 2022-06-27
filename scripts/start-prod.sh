9da162beb88f8a4923e2f7d4ea4e2b31e4220121
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-06-26 20:12:32 -0500

    :bug: Production Server not start
    Add gunicorn package, before the abcense of this package cause an error on try to run production image

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 
 alembic upgrade head
-gunicorn --bind 0.0.0.0:5000 app.main:app -k uvicorn.workers.UvicornWorker
+gunicorn --bind 0.0.0.0:8000 app.main:app -k uvicorn.workers.UvicornWorker
