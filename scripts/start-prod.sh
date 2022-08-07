25a6b624aa1bac4126092398a5fcecdfb3d2ad5e
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-08-06 19:31:38 -0500

    :sparkles: Performance Improvements
    Set 4 workers for gunicorn, to improve requests per second time

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 
 alembic upgrade head
-gunicorn --bind 0.0.0.0:8000 app.main:app -k uvicorn.workers.UvicornWorker
+gunicorn -w 4 --bind 0.0.0.0:8000 app.main:app -k uvicorn.workers.UvicornWorker
